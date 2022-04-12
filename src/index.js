0d2686f91b0922602d0f3986204e060270d477c8
Awais Amjed <awaisamjed171@gmail.com>
2022-04-12 16:16:55 +0500

    Fix Linter issues

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
